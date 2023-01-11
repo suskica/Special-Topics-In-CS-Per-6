ab1d601b8c25a78da3d0c0ee4031d59dcc8a9ba2
suskica <s-suskica@bsd405.org>
2023-01-11 12:01:16 -0800

    FIX THE NAME DUMMY

--- Scripts/EvenBetterRaceScripts.cs
+++ Scripts/EvenBetterRaceScripts.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BetterRaceScript : MonoBehaviour
+public class EvenBetterRaceScript : MonoBehaviour
 {
     private float totalTime;
     private float lapTime;
