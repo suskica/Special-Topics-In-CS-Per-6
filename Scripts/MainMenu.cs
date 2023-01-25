2c7d96134d57f8c282eff2ed41667555bf6a6cef
suskica <s-suskica@bsd405.org>
2023-01-25 11:51:55 -0800

    Comments For Main Menu

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -5,24 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    // Loads the Level One Scene from the Project Build using the Scene Manager
     public void LevelOne ()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("Car3");
     }
 
+    // Loads the Level Two Scene from the Project Build using the Scene Manager
     public void LevelTwo ()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("mySon1-18");
     }
 
+    // Loads the Level Three Scene from the Project Build using the Scene Manager
     public void StartMenu()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("Main Menu");
     }
 
+    // Loads the Main Menu Scene from the Project Build using the Scene Manager
     public void LevelThree()
     {
         Time.timeScale = 1f;
