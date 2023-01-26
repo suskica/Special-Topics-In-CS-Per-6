0f571f676529f3ac386f05aa1f4d1478fc27eaaf
Ryan Tiotuico <ryetyo@yahoo.com>
2023-01-26 15:23:49 -0800

    added comments to sizeup sizedown whowon

--- Scripts/SizeUp.cs
+++ Scripts/SizeUp.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class SizeUp : MonoBehaviour
 {
+    // Need variables for the particle effect, how big the car is supposed to grow and how long the effect should last
+     //which in this case is 5 sec
     public GameObject pickupEffect;
 
     public float multiplier = 1.4f;
 
     public float duration = 5f;
+
+    //a method that gets activated "on trigger" or when the car comes in contact with the powerup; will then call the Pickup method
     void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) {
             StartCoroutine(Pickup(other));
         }
     }
+
+    // spawn particle effect when pickup; increase the car size by the multiplier; 
+    //its an IEnumerator so we can wait a certain amount of seconds before the powerup effect stops; the powerup is then destroyed as well
     IEnumerator Pickup(Collider player) {
         Instantiate(pickupEffect, transform.position, transform.rotation);
 
